83810fdf7918d8714ab56dd6752592186c024cc1
Wangjie <m15205519363@163.com>
2024-11-15 18:03:11 +0800

    update

--- server.ts
+++ server.ts
@@ -4,7 +4,8 @@ import express from 'express';
 import { fileURLToPath } from 'node:url';
 import { dirname, join, resolve } from 'node:path';
 import bootstrap from './src/main.server';
-import api from 'api';
+import api from './api'; // 确保路径正确
+import cors from 'cors'; // 引入 cors
 
 // The Express app is exported so that it can be used by serverless Functions.
 export function app(): express.Express {
@@ -18,11 +19,18 @@ export function app(): express.Express {
   server.set('view engine', 'html');
   server.set('views', browserDistFolder);
 
+  // 使用 cors 中间件
+  server.use(cors());
+
+  // 日志记录
+  server.use((req, res, next) => {
+    console.log(`Request URL: ${req.url}`);
+    next();
+  });
+
   // 使用API处理文件
   server.use('/jayBlogApi', api);
 
-  // Example Express Rest API endpoints
-  // server.get('/api/**', (req, res) => { });
   // Serve static files from /browser
   server.get('**', express.static(browserDistFolder, {
     maxAge: '1y',
@@ -43,6 +51,12 @@ export function app(): express.Express {
       })
       .then((html) => res.send(html))
       .catch((err) => next(err));
+  });
+
+  // 错误处理
+  server.use((err: any, req: any, res: any, next: any) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
   });
 
   return server;
